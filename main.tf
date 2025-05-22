b4bcbcdafb4435cfd165c9637269310ea0c49c9f
sameermaner <106797445+sameermaner@users.noreply.github.com>
2025-05-22 06:23:09 +0530

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,22 @@
-provider "azurerm" {
-    features {}
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.29.0"
+    }
+  }
 }
+
+provider "azurerm" {
+  subscription_id = "853976da-1943-482f-be67-2d729c61ef50"
+  features {}
+}
+
 resource "azurerm_resource_group" "rg1" {
   name     = "rg1"
   location = "eastus"
 }
+
 resource "azurerm_resource_group" "rg2" {
   name     = "rg2"
   location = "westus"
