20fa50a2431a5fdbd6038b4dc421f964645ff0a1
sameermaner <106797445+sameermaner@users.noreply.github.com>
2025-05-22 06:57:28 +0530

    Only RG1

--- main.tf
+++ main.tf
@@ -17,12 +17,4 @@ resource "azurerm_resource_group" "rg1" {
   location = "eastus"
 }
 
-resource "azurerm_resource_group" "rg2" {
-  name     = "rg2"
-  location = "westus"
-}
 
-resource "azurerm_resource_group" "rg3" {
-  name     = "rg3"
-  location = "westus"
-}
